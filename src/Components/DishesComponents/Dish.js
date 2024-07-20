729ca2993f930691061d79495715a3069bce752b
MiLee777 <midyashka1506@gmail.com>
2024-07-20 11:27:18 +0700

    fix: fix bug quantity

--- src/Components/DishesComponents/Dish.js
+++ src/Components/DishesComponents/Dish.js
@@ -1,40 +1,23 @@
-import { useDispatch, useSelector } from "react-redux";
-import dishesData from "../../Data/DataDishes";
-import { getSelectedCategory } from "../../redux/dishesSlice";
+import { useDispatch } from "react-redux";
 import { ChangeQuantuty } from "../Cart/ChangeQuantity";
 import { useState } from "react";
 import { addItemToCart } from "../../redux/cartSlice";
 
-export const Dish = () => {
+export const Dish = ({ dish }) => {
 
   const [quantity, setQuantity] = useState(1);
-
-  const selectedCategory = useSelector(getSelectedCategory);
 
   const dispatch = useDispatch();
 
   return (
     <div>
-      {dishesData
-      .filter(dish => {
-        if(selectedCategory === 'ALL') return true;
-        return selectedCategory === dish.category;
-      })
-      .map(dish => {
-        const { id, name, price, img } = dish;
-        
-        return (
-          <div key={ id }>
-            <img className="image" src={ `../images/${img}.jpg`} alt="Meal" />
-            <h1>{ name }</h1>
-            <p>${ price }</p>
-            <ChangeQuantuty quantity={ quantity } setQuantity={ setQuantity } />
-            <button onClick={() => dispatch(addItemToCart({ dish, quantity }))}>
-              Add to cart
-            </button>
-          </div>
-        )
-      })}
+      <img className="image" src={ `../images/${dish.img}.jpg`} alt="Meal" />
+      <h1>{ dish.name }</h1>
+      <p>${ dish.price }</p>
+      <ChangeQuantuty quantity={ quantity } setQuantity={ setQuantity } />
+      <button onClick={() => dispatch(addItemToCart({ dish, quantity }))}>
+        Add to cart
+      </button>
     </div>
   )
 }
